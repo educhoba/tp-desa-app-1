eb6ab9c95ed48b685eb5946d51f9adae21cc52ce
MelanieVera <meelvera@gmail.com>
2024-05-29 18:27:48 -0300

    Front
    Subo front

---
+++ frontendMuni/MyApplication/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+}
+
+android {
+    namespace = "com.example.myapplication"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies{
+
+    implementation(libs.constraintlayout)
+    implementation(libs.ext.junit)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+
+
+}
